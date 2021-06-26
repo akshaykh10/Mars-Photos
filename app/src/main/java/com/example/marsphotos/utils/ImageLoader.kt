2991d43a9940c63d343b02b07f6300d94b3364d5
akshaykh10 <akshaykhemanth@gmail.com>
2021-06-26 14:25:20 +0530

    Final Commit

--- app/src/main/java/com/example/marsphotos/utils/ImageLoader.kt
+++ app/src/main/java/com/example/marsphotos/utils/ImageLoader.kt
@@ -5,34 +5,39 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.marsphotos.ui.theme.mars
 import com.google.accompanist.coil.rememberCoilPainter
 import com.google.accompanist.imageloading.ImageLoadState
 
+
+
+
 @Composable
 fun LoadImage(url:String){
     val painter= rememberCoilPainter(
         request=url,
-        fadeIn=true,
-        previewPlaceholder = R.drawable.placeholder
+        fadeIn=true
     )
-
 
     Box (modifier=Modifier.clip(shape=MaterialTheme.shapes.large)) {
         Image(
             painter =painter,
             contentDescription = null,
-            modifier= Modifier.fillMaxWidth().height(180.dp).clip(shape= MaterialTheme.shapes.large),
+            modifier= Modifier.fillMaxWidth()
+                .height(196.dp)
+                .clip(shape= MaterialTheme.shapes.large),
             contentScale = ContentScale.FillBounds
         )
+
 
         when (painter.loadState) {
             is ImageLoadState.Loading -> {
@@ -44,11 +49,13 @@ fun LoadImage(url:String){
                 )
             }
             is ImageLoadState.Error -> {
-                Text(
-                    "Error",
-                    color=MaterialTheme.colors.onBackground,
-                    modifier=Modifier.align(Alignment.Center)
-                )
+               Icon(
+                   painter=painterResource(R.drawable.ic_baseline_error_24),
+                   contentDescription = null,
+                   modifier=Modifier.align(Alignment.Center),
+                   tint=mars
+
+               )
             }
         }
     }
